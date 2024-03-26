5a89be4f23df51abb69e5d3159f4b200524c3b96
Muhammad Fajri <160698852+Fajri135@users.noreply.github.com>
2024-03-26 11:20:00 +0700

    189_Switch

---
+++ 189_Switch.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+int pembagian(int a, int b)
+{
+    return a / b;
+}
+
+void inputData()
+{
+    cout << "\nMasukan Bilangan Pertama :";
+    cin >> bilangan1;
+    cout << "Masukan Bilangan Kedua :";
+    cin >> bilangan2;
+}
+
+int main()
+{
+    int pilihan;
+    do
+    {
+        system("CLS");
+        cout << "Menu Kalkulator Sederhana" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukan Pilihan : ";
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case 1:
+            inputData();
+            cout << "\nHasil Penjumlahan: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 2:
+            inputData();
+            cout << "\nHasil Pengurangan: " << pengurangan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 3:
+            inputData();
+            cout << "\nHasil Perkalian: " << perkalian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 4:
+            inputData();
+            cout << "\nHasil Pembagian: " << pembagian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            inputData();
+            cout << "Pilihan tidak valid" << endl;
+            system("pause");
+            break;
+
+        }
+    } while (pilihan != 5);
+    return 0;
+}
